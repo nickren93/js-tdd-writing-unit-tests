277050c413c7cdb5ddc279e23d4b2b371c76eb1d
Sitong <nickren93@gmail.com>
2025-03-12 21:55:11 -0400

    1st working refactor

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
 // Your code here
 // src/utils.js
 export function pointsForWord(word) {
+    /*
+    below are our initial working code before refactor:
+
     let points = 0;
     for (const char of word) {
       if (["a", "e", "i", "o", "u"].includes(char)) {
@@ -8,6 +11,14 @@ export function pointsForWord(word) {
       } else {
         points += 2;
       }
+    }
+    return points;
+    */
+
+    //below are after refactor:
+    let points = 0;
+    for (const char of word) {
+        points += /[aeiou]/.test(char) ? 1 : 2;
     }
     return points;
   }
